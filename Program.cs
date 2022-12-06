99b91d4c3888369041e67ad1db151e13388acd1b
16kislyak <16kislyak@gmail.com>
2022-12-07 00:37:38 +0300

    function of finding elements with three symbol

--- Program.cs
+++ Program.cs
@@ -6,17 +6,45 @@
 string[] GetArray(int size)
 {
     string[] res = new string[size];
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        Console.Write($"Введите {i+1} элемент массива: ");
+        Console.Write($"Введите {i + 1} элемент массива: ");
         res[i] = (Console.ReadLine()!).ToString();
     }
     return res;
 }
 
+string[] ArrayThreeSymbol(string[] sourceArr)
+{
+    int leng = 0;
+    for (int i = 0; i < sourceArr.Length; i++)
+    {
+        if ((sourceArr[i].Length) <= 3)
+        {
+            leng++;
+        }
+    }
+    string[] finArray = new string[leng];
+    int count = 0;
+    for (int k = 0; k < sourceArr.Length; k++)
+    {
+        if ((sourceArr[k].Length) <= 3)
+        {
+            finArray[count] = sourceArr[k];
+            count++;
+        }
+    }
+    return finArray;
+}
+
 Console.Write("Введите количество элементов массива: ");
 int M = int.Parse(Console.ReadLine()!);
-string[] newArray = GetArray(M);
+string[] Array = GetArray(M);
+Console.Write("[");
+Console.Write(String.Join(", ", Array));
+Console.WriteLine("]");
+
+string[] newArray = ArrayThreeSymbol(Array);
 Console.Write("[");
 Console.Write(String.Join(", ", newArray));
 Console.WriteLine("]");
