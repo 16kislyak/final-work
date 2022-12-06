9489223227c3444898a1f30e3a65eb217c52c1f4
16kislyak <16kislyak@gmail.com>
2022-12-06 23:55:45 +0300

    creating the original array

--- Program.cs
+++ Program.cs
@@ -3,3 +3,20 @@
 // либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+string[] GetArray(int size)
+{
+    string[] res = new string[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива: ");
+        res[i] = (Console.ReadLine()!).ToString();
+    }
+    return res;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int M = int.Parse(Console.ReadLine()!);
+string[] newArray = GetArray(M);
+Console.Write("[");
+Console.Write(String.Join(", ", newArray));
+Console.WriteLine("]");
